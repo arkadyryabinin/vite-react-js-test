d59099d4a36f169f61a1aabcb0a1d5fccc42a026
Arkady Ryabinin <arkadyryabinin@yandex.ru>
2026-02-02 17:46:20 +0300

    tests

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
+
+
 function NewPost() {
+  const [state, setState] = useState('');
   function changeBodyHandler(event) {
-    console.log(event.target.value);
+    setState(event.target.value);
   }
 
   return (
@@ -11,6 +15,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p>{state}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
