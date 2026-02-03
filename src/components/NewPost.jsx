214480f91dfd43528d4c36513938d0350743bcd1
Arkady Ryabinin <arkadyryabinin@yandex.ru>
2026-02-03 18:12:50 +0300

    module 2-022

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,24 +1,43 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 
 
-function NewPost({ changeBodyHandler, cancel }) {
+function NewPost({ cancel }) {
+  const [author, setAuthor] = useState('');
+  const [text, setText] = useState('');
+  
+  function changeTextHandler(event) {
+    setText(event.target.value);
+  }
+  function changeAuthorHandler(event) {
+    setAuthor(event.target.value);
+  }
 
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: text,
+      author,
+    };
+    console.log(postData);
+    cancel();
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+        <textarea id="body" required rows={3} onChange={changeTextHandler}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={changeAuthorHandler}/>
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={cancel}>Cancel</button>
         <button>Submit</button>
       </p>
-    </form>
+    </form> 
   );
 }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,26 +1,19 @@
-import { useState } from 'react';
 import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function List({ toggleModal, isOpened }) {
-  const [state, setState] = useState('');
-  
-  function changeBodyHandler(event) {
-    setState(event.target.value);
-  }
   return (
     <>
       {
         isOpened && (
           <Modal>
-            <NewPost changeBodyHandler={changeBodyHandler} cancel={toggleModal} />
+            <NewPost cancel={toggleModal} />
           </Modal>
         )
       }
       <ul className={classes.posts}>
-        <Post author="Arkady" body={state} />
         <Post author="Oxana" body="Hello World" />
       </ul>
     </>
